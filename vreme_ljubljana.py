edf9f2f633d47522972cc5cd62fd0e1d68e3fcb0
UrosPlut <63784958+UrosPlut@users.noreply.github.com>
2021-02-02 09:40:25 +0100

    Add files via upload

---
+++ vreme_ljubljana.py
@@ -0,0 +1,15 @@
+import http.client
+
+conn = http.client.HTTPSConnection("community-open-weather-map.p.rapidapi.com")
+
+headers = {
+    'x-rapidapi-key': "4cb395500bmsh7ca4738812909a8p127940jsnab3936735aec",
+    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com"
+    }
+
+conn.request("GET", "/weather?q=Ljubljana&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22&mode=xml%2C%20html", headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+
+print("Ljubljana: ",data.decode("utf-8"))
